ddecbf9ca7169a32f844585c43bd045c95cfcb15
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2019-01-10 15:03:41 +0100

    Made instruction

--- app/src/main/java/com/example/hvalfisk/notsosuckyhangman/InstructionsActivity.java
+++ app/src/main/java/com/example/hvalfisk/notsosuckyhangman/InstructionsActivity.java
@@ -2,12 +2,47 @@ package com.example.hvalfisk.notsosuckyhangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class InstructionsActivity extends AppCompatActivity {
+
+    View instructionWords;
+    View instructionGuessing;
+    View instructionWinning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        instructionWords = findViewById(R.id.instructionWords);
+        instructionGuessing = findViewById(R.id.instructionGuessing);
+        instructionWinning = findViewById(R.id.instructionWinning);
+
+        insertText(instructionWords, "words");
+        insertText(instructionGuessing, "guessing");
+        insertText(instructionWinning, "winning");
+    }
+
+    private void insertText(View instruction, String instructionKey) {
+        TextView header = (TextView) instruction.findViewById(R.id.instructionHeadline);
+        TextView body = (TextView) instruction.findViewById(R.id.instructionBody);
+
+        switch(instructionKey) {
+            case "words":
+                header.setText(getResources().getString(R.string.instruction_header_words));
+                body.setText(getResources().getString(R.string.instruction_words));
+                break;
+            case "guessing":
+                header.setText(getResources().getString(R.string.instruction_header_guessing));
+                body.setText(getResources().getString(R.string.instruction_guessing));
+                break;
+            case "winning":
+                header.setText(getResources().getString(R.string.instruction_header_winning));
+                body.setText(getResources().getString(R.string.instruction_winning));
+                break;
+
+        }
     }
 }
